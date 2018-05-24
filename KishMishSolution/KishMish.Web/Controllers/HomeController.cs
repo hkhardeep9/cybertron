58c8a118e80c4a26549390ca6ff236618dd95b21
vaibhavbharakhada1992 <vaibhavbharakhada1992@gmail.com>
2018-05-24 23:06:08 +0530

    24th may

---
+++ KishMishSolution/KishMish.Web/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KishMish.Data.BusinessAccessLayer;
+using KishMish.Web.Models.Traders;
+
+namespace KishMish.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if(Session["username"] != null)
+            Response.Write(Session["username"].ToString());
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            Response.Write("invalid user");
+
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Index(VMTraders traders)
+        {
+            if (ModelState.IsValid)
+            {
+                if (BATraders.BA_AuthenticateLoginDetails(traders.UserName, traders.Password))
+                {
+                    ViewBag.username = "logggedd innn";
+                }
+                else { ViewBag.username = " not logggedd innn"; }              
+            }
+            return View();
+        }
+
+    }
+}
---
+++ KishMishSolution/KishMish.Web/Models/Traders/VMTraders.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KishMish.Web.Models.Traders
+{
+    public class VMTraders
+    {
+        private string name;
+        private string userName;
+        private string password;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+
+            set
+            {
+                userName = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+    }
+}
